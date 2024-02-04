619a3a484c9d021ab4fa95483fb1fc3b3814b5d9
Sanja Petrović <sanjica.petrovic7@gmail.com>
2024-02-04 18:11:35 +0100

    update README.md

--- artifacts/channel/run_configtx.sh
+++ artifacts/channel/run_configtx.sh
@@ -4,5 +4,3 @@
   mkdir config
 
   ../../bin/configtxgen -profile FourOrgsOrdererGenesis -outputBlock ./config/genesis.block -channelID testchannelid
-  #../../bin/configtxgen -profile FourOrgsOrdererGenesis -outputCreateChannelTx ./config/channel.tx -channelID $CHANNEL_NAME
-  #../../bin/configtxgen -profile FourOrgsOrdererGenesis -outputAnchorPeersUpdate ./config/${MSP_NAME}anchors.tx -channelID $CHANNEL_NAME -asOrg $MSP_NAME
