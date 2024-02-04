4c7ccd84fcac9bdc94a12a2445f1a9f11d2081e0
Sanja Petrović <sanjica.petrovic7@gmail.com>
2024-02-04 19:39:28 +0100

    add script for creating channels

--- artifacts/channel/run_configtx.sh
+++ artifacts/channel/run_configtx.sh
@@ -4,3 +4,13 @@
   mkdir config
 
   ../../bin/configtxgen -profile FourOrgsOrdererGenesis -outputBlock ./config/genesis.block -channelID testchannelid
+  ../../bin/configtxgen -profile FourOrgsChannel1 -outputCreateChannelTx ./config/channel1.tx -channelID channel1
+  ../../bin/configtxgen -profile FourOrgsChannel2 -outputCreateChannelTx ./config/channel2.tx -channelID channel2
+  ../../bin/configtxgen -profile FourOrgsChannel1 -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID channel1 -asOrg Org1MSP
+  ../../bin/configtxgen -profile FourOrgsChannel1 -outputAnchorPeersUpdate ./config/Org2MSPanchors.tx -channelID channel1 -asOrg Org2MSP
+  ../../bin/configtxgen -profile FourOrgsChannel1 -outputAnchorPeersUpdate ./config/Org3MSPanchors.tx -channelID channel1 -asOrg Org3MSP
+  ../../bin/configtxgen -profile FourOrgsChannel1 -outputAnchorPeersUpdate ./config/Org4MSPanchors.tx -channelID channel1 -asOrg Org4MSP
+  ../../bin/configtxgen -profile FourOrgsChannel2 -outputAnchorPeersUpdate ./config/Org1MSPanchors.tx -channelID channel2 -asOrg Org1MSP
+  ../../bin/configtxgen -profile FourOrgsChannel2 -outputAnchorPeersUpdate ./config/Org2MSPanchors.tx -channelID channel2 -asOrg Org2MSP
+  ../../bin/configtxgen -profile FourOrgsChannel2 -outputAnchorPeersUpdate ./config/Org3MSPanchors.tx -channelID channel2 -asOrg Org3MSP
+  ../../bin/configtxgen -profile FourOrgsChannel2 -outputAnchorPeersUpdate ./config/Org4MSPanchors.tx -channelID channel2 -asOrg Org4MSP
