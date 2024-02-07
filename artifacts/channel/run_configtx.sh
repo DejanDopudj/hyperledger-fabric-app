3a2881b22cd1de93ff13f8c371526919b788d1be
Sanja Petrović <85809173+sanja-petrovic@users.noreply.github.com>
2024-02-07 18:34:56 +0100

    Feature/channels (#3)
    * add script for creating channels
    
    * create channel config
    
    * update .gitignore
    
    * specify bin files

--- artifacts/channel/run_configtx.sh
+++ artifacts/channel/run_configtx.sh
@@ -3,4 +3,14 @@
   fi
   mkdir config
 
-  ../../bin/configtxgen -profile FourOrgsOrdererGenesis -outputBlock ./config/genesis.block -channelID testchannelid
+  ../../bin/configtxgen -configPath=. -profile FourOrgsOrdererGenesis -outputBlock ./config/genesis.block -channelID testchannelid
+  # ../../bin/configtxgen -configPath=. -profile FourOrgsChannel -outputCreateChannelTx ./config/channel1.tx -channelID channel1
+  # ../../bin/configtxgen -configPath=. -profile FourOrgsChannel -outputCreateChannelTx ./config/channel2.tx -channelID channel2
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors-1.tx -channelID channel1 -asOrg Org1MSP
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org2MSPanchors-1.tx -channelID channel1 -asOrg Org2MSP
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org3MSPanchors-1.tx -channelID channel1 -asOrg Org3MSP
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org4MSPanchors-1.tx -channelID channel1 -asOrg Org4MSP
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org1MSPanchors-2.tx -channelID channel2 -asOrg Org1MSP
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org2MSPanchors-2.tx -channelID channel2 -asOrg Org2MSP
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org3MSPanchors-2.tx -channelID channel2 -asOrg Org3MSP
+  # ../../bin/configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./config/Org4MSPanchors-2.tx -channelID channel2 -asOrg Org4MSP
---
+++ artifacts/up-ca-and-orderer.sh
@@ -0,0 +1,2 @@
+docker compose up ca.org1.example.com ca.org2.example.com ca.org3.example.com ca.org4.example.com -d
+docker compose up orderer.example.com
