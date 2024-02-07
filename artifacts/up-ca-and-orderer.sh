8e46a314868e814d7fa4147a1ae3ad295b54e99f
dejand <dejand@nordeus.com>
2024-02-07 18:48:11 +0100

    Add test-network

--- artifacts/up-ca-and-orderer.sh
+++
@@ -1,2 +0,0 @@
-docker compose up ca.org1.example.com ca.org2.example.com ca.org3.example.com ca.org4.example.com -d
-docker compose up orderer.example.com
--- artifacts/up-org1.sh
+++
@@ -1,1 +0,0 @@
-docker compose up peer0.org1.example.com peer1.org1.example.com peer2.org1.example.com peer3.org1.example.com
